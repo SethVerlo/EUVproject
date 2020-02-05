9796e37f2aff063f958b5e3da19c68c36e5854ba
Chang Liu <34147138+SethVerlo@users.noreply.github.com>
2020-02-05 12:06:14 +0900

    Add files via upload

---
+++ population.py
@@ -0,0 +1,11 @@
+import ChiantiPy.core as ch
+import numpy as np
+import ChiantiPy.tools.filters as chfilters
+import matplotlib.pyplot as plt
+
+t = 10.**(3.84 + 0.05*np.arange(8.))
+H1 = ch.ion('h_1', temperature=t, eDensity=1.e+14, em=1.e+27)
+H1.popPlot()
+H1.intensityPlot(wvlRange=[6564,6565],linLog='log', index=4)
+H1.intensityList(wvlRange=[4000,7000], index=4)
+plt.show(H1)
