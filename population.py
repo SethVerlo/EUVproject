3bdd31404280806f5ae771e422ef82ca15029ea5
Chang Liu <34147138+SethVerlo@users.noreply.github.com>
2020-02-18 11:57:26 +0900

    Update population.py

--- population.py
+++ population.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 t = 10.**(3.84 + 0.05*np.arange(8.))
 H1 = ch.ion('h_1', temperature=t, eDensity=1.e+14, em=1.e+27)
 H1.popPlot(top=10)
-H1.intensityPlot(wvlRange=[6564,6565],linLog='log', index=4)
-H1.intensityList(wvlRange=[4000,7000], index=4)
+H1.intensityPlot(wvlRange=[6564,6565],linLog='log', index=4, top=10)
+H1.intensityList(wvlRange=[4000,7000], index=4, top=10)
 plt.show(H1)
 
 H1a = ch.ioneq(1)
